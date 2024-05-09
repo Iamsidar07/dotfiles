de016ab01d793defcbc58edaa41f7d2657fb9860
iamsidar07 <ms8460149@gmail.com>
2024-05-09 18:27:54 +0530

    initial commit

---
+++ .config/nvim/lua/plugins/nightfly.lua
@@ -0,0 +1,9 @@
+return {
+  { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nightfly",
+    },
+  },
+}
